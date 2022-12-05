ab8f29bb3329fb2a72abf1be90e9ce3c92756fd3
Stasy <mamontova.nasta@yandex.ru>
2022-12-05 20:59:57 +0300

    SortingTime

---
+++ src/main/java/TicketTimeComparator.java
@@ -0,0 +1,9 @@
+import java.util.Comparator;
+
+public class TicketTimeComparator implements Comparator<Ticket> {
+
+    @Override
+    public int compare(Ticket o1, Ticket o2) {
+        return o1.getTime() - o2.getTime();
+    }
+}
